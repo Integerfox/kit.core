21dee9598346495ccca549451df733215083816e
John Taylor <john.t.taylor@gmail.com>
2025-07-26 22:58:31 -0400

    tweaks

--- src/Kit/Container/_0test/_0build/windows/msvc/build_catch2.py
+++ src/Kit/Container/_0test/_0build/windows/msvc/build_catch2.py
@@ -5,10 +5,11 @@ import sys
 
 # MAIN
 if __name__ == '__main__':
-    # Run the "real" script
+    # Get the xpackage root directory from environment variable
     xpackage_root  = os.environ.get('NQBP_XPKGS_ROOT')
 
-    script = os.path.join(xpackage_root, "nqbp2", "other", "compile_catch2_base.py" )
+    # Run the "real" script
+    script = os.path.join( xpackage_root, "nqbp2", "other", "compile_catch2_base.py" )
     cmd = script + " " + " ".join(sys.argv[1:])
     rc = os.system( cmd )
     sys.exit( rc )
