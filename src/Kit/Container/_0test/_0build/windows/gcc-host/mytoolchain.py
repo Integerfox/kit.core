2ee9ad5b4a113d23b6f8586647fdf302b5add9e5
John Taylor <john.t.taylor@gmail.com>
2025-07-27 12:26:25 -0400

    mingw build working

--- src/Kit/Container/_0test/_0build/windows/gcc-host/mytoolchain.py
+++ src/Kit/Container/_0test/_0build/windows/gcc-host/mytoolchain.py
@@ -6,27 +6,30 @@
 #    - ONLY edit/add statements in the sections marked by BEGIN/END EDITS
 #      markers.
 #    - Maintain indentation level and use spaces (it's a python thing) 
-#    - rvalues must be enclosed in quotes (single ' ' or double " ")
 #    - The structure/class 'BuildValues' contains (at a minimum the
 #      following data members.  Any member not specifically set defaults
 #      to null/empty string
-#            .inc 
-#            .asminc
-#            .cflags
-#            .cppflags
-#            .asmflags
-#            .linkflags
-#            .linklibs
+#            .inc             # C/C++ search path include directories 
+#            .asminc          # Assembly search path include directories
+#            .c_only_flags    # C only compiler flags
+#            .cflags          # C and C++ compiler flags
+#            .cppflags        # C++ only compiler flags
+#            .asmflags        # Assembly compiler flags
+#            .linkflags       # Linker flags
+#            .linklibs        # Linker libraries
+#            .firstobjs       # Object files to be unconditionally linked first
+#            .lastobjs        # Object files to be unconditionally linked last
 #           
 #---------------------------------------------------------------------------
 
 # get definition of the Options structure
 from nqbplib.base import BuildValues
-from nqbplib.my_globals import NQBP_PKG_ROOT
-from nqbplib.my_globals import NQBP_PRE_PROCESS_SCRIPT
-from nqbplib.my_globals import NQBP_PRE_PROCESS_SCRIPT_ARGS
-import os, copy
+from nqbplib.my_globals import *
+from nqbplib.utils import config_catch2
+import os, copy 
 
+# Capture project/build directory
+prjdir = os.path.dirname(os.path.abspath(__file__))
 
 #===================================================
 # BEGIN EDITS/CUSTOMIZATIONS
@@ -35,35 +38,8 @@ import os, copy
 # Set the name for the final output item
 FINAL_OUTPUT_NAME = 'a.exe'
 
-# Link unittest directory by object module so that Catch's self-registration mechanism 'works'
-unit_test_objects = '_BUILT_DIR_.src/Cpl/Container/_0test'
-
-# Use Catch2 as a static library
-catch2_inc  = f'-I{os.path.join( NQBP_PKG_ROOT(), "xsrc", "catch2", "src" )}'
-catch2_lib  = f'{os.path.join( NQBP_PKG_ROOT(), "projects", "xsrc", "catch2", "lib", "windows", "mingw_w64", "_BUILD_VARIANT_DIR_", "catch2.a" )}'
-
-NQBP_PRE_PROCESS_SCRIPT( 'preprocess.py' )
-NQBP_PRE_PROCESS_SCRIPT_ARGS( r'windows\mingw_w64' )
-
-
-#
-# For build config/variant: "win32"
-#
-
-# Set project specific 'base' (i.e always used) options
-base_win32           = BuildValues()        # Do NOT comment out this line
-base_win32.cflags    = '-m32 -std=c++17 -Wall -Werror -x c++  -fprofile-arcs -ftest-coverage'
-base_win32.inc       = catch2_inc
-base_win32.linkflags = '-m32 -fprofile-arcs'
-base_win32.linklibs  = f'-lgcov {catch2_lib}'
-base_win32.firstobjs = unit_test_objects
-
-# Set project specific 'optimized' options
-optimized_win32           = BuildValues()    # Do NOT comment out this line
-optimized_win32.cflags    = '-O3'
-
-# Set project specific 'debug' options
-debug_win32           = BuildValues()       # Do NOT comment out this line
+# Using Catch2 
+(catch2_inc, catch2_lib, unit_test_objects) = config_catch2( prjdir, r'windows\gcc-host', 'a' )
 
 
 
@@ -90,18 +66,28 @@ optimized_win64.cflags    = '-O3'
 
 
 #
-# For build config/variant: "cpp11"
+# For build config/variant: "win32"
 #
 
-# same options as win64 (but uses different libdirs entries)
-base_cpp11      = copy.deepcopy(base_win64)
-optimized_cpp11 = copy.deepcopy(optimized_win64)
-debug_cpp11     = copy.deepcopy(debug_win64)
+# Set project specific 'base' (i.e always used) options
+base_win32           = BuildValues()        # Do NOT comment out this line
+base_win32.cflags    = '-m32 -std=c++17 -Wall -Werror -x c++  -fprofile-arcs -ftest-coverage'
+base_win32.inc       = catch2_inc
+base_win32.linkflags = '-m32 -fprofile-arcs'
+base_win32.linklibs  = f'-lgcov {catch2_lib}'
+base_win32.firstobjs = unit_test_objects
+
+# Set project specific 'optimized' options
+optimized_win32           = BuildValues()    # Do NOT comment out this line
+optimized_win32.cflags    = '-O3'
+
+# Set project specific 'debug' options
+debug_win32           = BuildValues()       # Do NOT comment out this line
+
 
 #-------------------------------------------------
-# ONLY edit this section if you are ADDING options
-# for build configurations/variants OTHER than the
-# 'release' build
+# ONLY edit this section if you are have more than
+# ONE build configuration/variant 
 #-------------------------------------------------
 
 win32_opts = { 'user_base':base_win32, 
@@ -115,17 +101,11 @@ win64_opts = { 'user_base':base_win64,
                'user_debug':debug_win64
              }
                
-# Add new dictionary of for new build configuration options
-cpp11_opts = { 'user_base':base_win64,
-               'user_optimized':optimized_win64,
-               'user_debug':debug_win64
-             }
         
 # Add new variant option dictionary to # dictionary of 
 # build variants
 build_variants = { 'win32':win32_opts,
                    'win64':win64_opts,
-                   'cpp11':cpp11_opts,
                  }    
 
 
@@ -133,18 +113,11 @@ build_variants = { 'win32':win32_opts,
 # END EDITS/CUSTOMIZATIONS
 #===================================================
 
-
-
-# Capture project/build directory
-import os
-prjdir = os.path.dirname(os.path.abspath(__file__))
-
-
 # Select Module that contains the desired toolchain
 from nqbplib.toolchains.windows.mingw_w64.console_exe import ToolChain
 
 
 # Function that instantiates an instance of the toolchain
 def create():
-    tc = ToolChain( FINAL_OUTPUT_NAME, prjdir, build_variants, "win32" )
+    tc = ToolChain( FINAL_OUTPUT_NAME, prjdir, build_variants, "win64" )
     return tc 
--- src/Kit/Container/_0test/_0build/windows/msvc/kit_config.h
+++ src/Kit/Container/_0test/_0build/windows/msvc/kit_config.h
@@ -1,16 +1,13 @@
-#ifndef COLONY_CONFIG_H_
-#define COLONY_CONFIG_H_
-
-// For C tests
-#define CPL_CONTAINER_ITEM_FATAL_ERROR_HANDLER myFatalErrorHandler
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-extern void myFatalErrorHandler( void* item, void* currentList, void* newList );
-
-#ifdef __cplusplus
-}
-#endif
+#ifndef KIT_CONFIG_H_
+#define KIT_CONFIG_H_
+/*-----------------------------------------------------------------------------
+ * Copyright Integer Fox Authors
+ *
+ * Distributed under the BSD 3 Clause License. See the license agreement at:
+ * https://github.com/Integerfox/kit.core/blob/main/LICENSE
+ *
+ * Redistributions of the source code must retain the above copyright notice.
+ *----------------------------------------------------------------------------*/
+/** @file Project/build specific 'Options' (see LConfig Pattern) */
 
 #endif
