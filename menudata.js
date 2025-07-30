e164ea37de21916a4c7a389387b804db9ef028bb
johnttaylor <johnttaylor@users.noreply.github.com>
2025-07-30 02:03:07 +0000

    deploy: 7bcc9c48560c03a9d2ab6169b8290ebe75b191b3

--- menudata.js
+++ menudata.js
@@ -25,6 +25,8 @@
 var menudata={children:[
 {text:"Main Page",url:"index.html"},
 {text:"Related Pages",url:"pages.html"},
+{text:"Namespaces",url:"namespaces.html",children:[
+{text:"Namespace List",url:"namespaces.html"}]},
 {text:"Components",url:"annotated.html",children:[
 {text:"Components List",url:"annotated.html"},
 {text:"Class Index",url:"classes.html"},
