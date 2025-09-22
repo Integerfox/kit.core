e5d1d4a930ed25a3864cb81f1e724b766e8fd8ab
johnttaylor <johnttaylor@users.noreply.github.com>
2025-09-22 12:08:33 +0000

    deploy: a9294b50624bd5185da4b72f105e47aaf5c997ff

--- doxygen/menudata.js
+++ doxygen/menudata.js
@@ -83,11 +83,13 @@ var menudata={children:[
 {text:"w",url:"functions_func_w.html#index_w"},
 {text:"~",url:"functions_func_~.html#index__7E"}]},
 {text:"Variables",url:"functions_vars.html",children:[
+{text:"a",url:"functions_vars.html#index_a"},
 {text:"c",url:"functions_vars.html#index_c"},
 {text:"d",url:"functions_vars.html#index_d"},
 {text:"i",url:"functions_vars.html#index_i"},
 {text:"m",url:"functions_vars.html#index_m"},
 {text:"n",url:"functions_vars.html#index_n"},
+{text:"s",url:"functions_vars.html#index_s"},
 {text:"t",url:"functions_vars.html#index_t"}]},
 {text:"Typedefs",url:"functions_type.html"},
 {text:"Enumerations",url:"functions_enum.html"},
