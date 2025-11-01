92800959d4f68cea7fa1c5b3f8a68a7015826ba6
John Taylor <john.t.taylor@gmail.com>
2025-11-01 14:19:42 -0400

    restore original Watchdog namespace

---
+++ src/Kit/System/Watchdog/Hal.h
@@ -0,0 +1,42 @@
+#ifndef KIT_SYSTEM_WATCHDOG_HAL_H_
+#define KIT_SYSTEM_WATCHDOG_HAL_H_
+/*------------------------------------------------------------------------------
+ * Copyright Integer Fox Authors
+ *
+ * Distributed under the BSD 3 Clause License. See the license agreement at:
+ * https://github.com/Integerfox/kit.core/blob/main/LICENSE
+ *
+ * Redistributions of the source code must retain the above copyright notice.
+ *----------------------------------------------------------------------------*/
+/** @file 
+
+    Hardware Abstraction Layer (HAL) for watchdog functionality
+
+*/
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    /** Enables the watchdog hardware.
+        @return true if watchdog was successfully enabled, false otherwise
+     */
+    bool Kit_System_Watchdog_hal_enable_wdog( void );
+
+    /** Kicks (resets/feeds) the watchdog to prevent it from timing out.
+        This should be called periodically when all monitored threads are healthy.
+     */
+    void Kit_System_Watchdog_hal_kick_wdog( void );
+
+    /** Trips the watchdog to force a system reset. This is called
+        when a monitored thread has failed.
+     */
+    void Kit_System_Watchdog_hal_trip_wdog( void );
+
+#ifdef __cplusplus
+}
+#endif
+#endif  // end header latch
