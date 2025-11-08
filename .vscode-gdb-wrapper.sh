3c97d9e809be919c9c22123889abcba3a7b64c23
John Taylor <john.t.taylor@gmail.com>
2025-11-08 10:55:47 -0500

    update github actions for the new STM32 builds

--- .vscode-gdb-wrapper.sh
+++ .vscode-gdb-wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # GDB wrapper script to setup environment
-source "$(dirname "$0")/env.sh" 2
+source "$(dirname "$0")/env.sh" 1
 gdb "$@"
---
+++ top/gha_linux_stm-arm.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+# This script is used by the CI/Build machine to build the Linux projects
+#
+# The script ASSUMES that the working directory is the package root
+#
+# usage: gha_linux_stm-arm.sh <buildNumber>
+# 
+
+# setup the environment
+source ./env.sh 1
+
+# Get script directory and set paths
+_TOPDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+_ROOT="$_TOPDIR/.."
+BUILD_NUMBER="$1"
+
+echo "Root:          $_ROOT"
+echo "Top:           $_TOPDIR"
+echo "Build Number:  $BUILD_NUMBER"
+
+#
+# Build STM projects
+#
+# Set up the compiler (which is included in the repo)
+source ./env.sh 5
+
+# Set the CI build flag
+export NQBP_CI_BUILD="1"
+
+# Build the projects
+pushd projects
+$NQBP_BIN/other/bob.py -v4 --p2 linux gcc-arm-mcu -c --bldtime --bld-all --bldnum "$BUILD_NUMBER"
+popd
+
+# Build the unit tests
+pushd src
+$NQBP_BIN/other/bob.py -v4 --p2 linux gcc-arm-mcu -c --bldtime --bld-all --bldnum "$BUILD_NUMBER"
+popd
