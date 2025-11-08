6bd035a3f3d9274f076454850ac26f2ada64a894
John Taylor <john.t.taylor@gmail.com>
2025-11-08 10:04:45 -0500

    get windows builds working

--- top/compilers/05-stm32-gcc-arm.bat
+++ top/compilers/05-stm32-gcc-arm.bat
@@ -1,9 +1,15 @@
-echo off
-IF "/%1"=="/name" ECHO:GCC-ARM (none-eabi) compiler for STM32 & exit /b 0
+@echo off
+:: Updated PATH for GCC ARM R/M Cross compiler
+set _CC_PATH_=\compilers\gcc-arm-none-eabi\bin
+IF "/%NQBP2_DONOT_ADD_GCC_ARM_TO_PATH%" == "/" set PATH=%_CC_PATH_%;%PATH% & set NQBP2_DONOT_ADD_GCC_ARM_TO_PATH=true
+
+:: Get the version from the compiler itself
+set _CC_VER=
+for /f "tokens=8 delims=) " %%a in ('arm-none-eabi-gcc --version 2^>nul ^| findstr /R /C:"^arm-none-eabi-gcc"') do set _CC_VER=%%a
 
 :: Segger support
 set PATH=%PATH%;c:\Program Files (x86)\SEGGER\JLink
 set PATH=%PATH%;c:\Program Files\SEGGER\Ozone
 
-set PATH=%NQBP_XPKGS_ROOT%\stm32-gcc-arm\bin;%PATH%
-echo:GCC Arm-none-eabi Environment set to xsrc\stm32-gcc-arm\bin
+:: Display the version info
+echo:GCC Arm-none-eabi Environment set. Ver=%_CC_VER% 
