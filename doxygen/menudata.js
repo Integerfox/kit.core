4dd2995afc1e3be4481cc8db97d7a6cb3af35cda
johnttaylor <johnttaylor@users.noreply.github.com>
2026-06-21 20:29:37 +0000

    deploy: 9dd6ad5173a04c42ee87a7263a8ae56d32bbfc5d

--- doxygen/menudata.js
+++ doxygen/menudata.js
@@ -42,7 +42,6 @@ var menudata={children:[
 {text:"l",url:"namespacemembers.html#index_l"},
 {text:"m",url:"namespacemembers.html#index_m"},
 {text:"n",url:"namespacemembers.html#index_n"},
-{text:"o",url:"namespacemembers.html#index_o"},
 {text:"p",url:"namespacemembers.html#index_p"},
 {text:"q",url:"namespacemembers.html#index_q"},
 {text:"r",url:"namespacemembers.html#index_r"},
@@ -173,8 +172,13 @@ var menudata={children:[
 {text:"r",url:"globals_r.html#index_r"},
 {text:"s",url:"globals_s.html#index_s"},
 {text:"u",url:"globals_u.html#index_u"},
-{text:"v",url:"globals_v.html#index_v"}]},
-{text:"Functions",url:"globals_func.html"},
+{text:"v",url:"globals_v.html#index_v"},
+{text:"w",url:"globals_w.html#index_w"}]},
+{text:"Functions",url:"globals_func.html",children:[
+{text:"b",url:"globals_func.html#index_b"},
+{text:"k",url:"globals_func.html#index_k"},
+{text:"m",url:"globals_func.html#index_m"},
+{text:"w",url:"globals_func.html#index_w"}]},
 {text:"Variables",url:"globals_vars.html"},
 {text:"Macros",url:"globals_defs.html",children:[
 {text:"_",url:"globals_defs.html#index__5F"},
