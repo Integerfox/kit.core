9f59bcfd9e954b41e99fa910c797dc47e56d3c39
John Taylor <john.t.taylor@gmail.com>
2026-08-30 17:48:24 -0600

    fix build errors on Windows

--- src/Kit/Driver/Flash/_0test/_0build/windows/msvc/kit_map.h
+++ src/Kit/Driver/Flash/_0test/_0build/windows/msvc/kit_map.h
@@ -1,7 +1,10 @@
 /** Note: Intentionally there is NO Header latch (see LHeader Pattern) */
 
-// IO mapping.  Note: Needs to be included FIRST because of winsock2.h ordering issues
+// IO mapping.  Note: Needs to be include FIRST because of winsock2.h ordering issues
 #include "Kit/Io/_mappings/_win32/mappings.h"
 
 // OSAL mappings
 #include "Kit/System/Win32/mappings.h"
+
+// strapi mapping
+#include "Kit/Text/_mappings/_msvc/strapi.h"
